c1e524c97a3ef52abead2972a1f391ab6e047451
cfree14 <cfree14@gmail.com>
2019-05-28 08:28:12 -0700

    added script to format CA landings data

---
+++ data/format_landings_data.R
@@ -0,0 +1,16 @@
+
+# Clear workspace
+rm(list = ls())
+
+# Setup
+################################################################################
+
+# Packages
+library(tidyverse)
+
+# Directories
+inputdir <- "data/raw/CA Landings Data"
+outputdir <- "data/processed"
+
+# Read data
+
